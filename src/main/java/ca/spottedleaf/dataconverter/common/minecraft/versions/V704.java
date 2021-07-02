81e3840ae49b439d25d5d936a66217ba59ff80c5
Spottedleaf <Spottedleaf@users.noreply.github.com>
2021-07-01 19:59:14 -0700

    Fix NPE for BlockEntity air inside air itemstack
    Move the check for logging inside, the else branch
    tried to update the id - but it was null.

--- src/main/java/ca/spottedleaf/dataconverter/common/minecraft/versions/V704.java
+++ src/main/java/ca/spottedleaf/dataconverter/common/minecraft/versions/V704.java
@@ -259,8 +259,10 @@ public final class V704 {
                 }
 
                 final boolean removeId;
-                if (entityId == null && !"minecraft:air".equals(itemId)) {
-                    LOGGER.warn("Unable to resolve Entity for ItemStack (V704): " + itemId);
+                if (entityId == null) {
+                    if (!"minecraft:air".equals(itemId)) {
+                        LOGGER.warn("Unable to resolve Entity for ItemStack (V704): " + itemId);
+                    }
                     removeId = false;
                 } else {
                     removeId = !entityTag.hasKey("id", ObjectType.STRING);
@@ -285,8 +287,10 @@ public final class V704 {
                 final String itemId = data.getString("id");
                 final String entityId = ITEM_ID_TO_TILE_ENTITY_ID.get(itemId);
                 final boolean removeId;
-                if (entityId == null && !"minecraft:air".equals(itemId)) {
-                    LOGGER.warn("Unable to resolve BlockEntity for ItemStack (V704): " + itemId);
+                if (entityId == null) {
+                    if (!"minecraft:air".equals(itemId)) {
+                        LOGGER.warn("Unable to resolve BlockEntity for ItemStack (V704): " + itemId);
+                    }
                     removeId = false;
                 } else {
                     removeId = !blockEntityTag.hasKey("id", ObjectType.STRING);
